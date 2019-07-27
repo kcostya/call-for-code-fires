ef6ab158594ed6ce4e9e2172ecb53a9507a0fe95
Rachael S <rachaels@rachaels-mbp.home>
2019-07-27 15:29:40 +0100

    updated colorscale and added utils file

---
+++ assets/dash_v2/utils.py
@@ -0,0 +1,14 @@
+import dash_html_components as html
+
+
+## some utility functions ##
+def generate_table(dataframe, max_rows):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
